a160f6b4714ec27bd02ad5db104478563ef02b70
Idyllic-Impala <46198661+Idyllic-Impala@users.noreply.github.com>
2018-12-27 12:04:49 -0500

    Add files via upload

---
+++ Lesson 1/First_Lesson.php
@@ -0,0 +1,6 @@
+<?php
+$string= '10';
+$int= '23';
+$float= 23.56;
+echo $string+$int;
+ ?>
---
+++ Lesson 1/If_Statements.php
@@ -0,0 +1,10 @@
+<?php
+if(1+1==3){
+  echo 'TRUE!';
+}elseif (1+1==4){
+  echo "TRUE!";
+}else{
+  echo "FALSE!";
+}
+
+ ?>
